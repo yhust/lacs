5d3e89d5d14d559f05b1f03aebe8caf7fd73293f
root <root@ip-172-31-73-109.ec2.internal>
2018-07-23 09:14:12 +0000

    scripts

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
 
-# Usage: $1 means the client number, $2 means the total request number each clientshall submit, $3 the request arrival rate on each client
+$total_count=$1   # total count of access
+$rate=$2 # access rate
+$factor=$3 # ratio of normal user and aggressive user
+
 read -ra slave_arr -d '' <<<"$SLAVES"
 
-SCRIPT="cd alluxio-load-balancing;PATH=$PATH python python/SPBenchmark.py $2 $3 > /tmp/pythonlog &"
-echo $SCRIPT
+#SCRIPT="cd alluxio-la;PATH=$PATH python python/LABenchmark.py $2 $3 > /tmp/pythonlog &"
+#echo $SCRIPT
 
-for ((i = 30; i < $1 + 30; i++))
-do
-    echo $i
-    slave="${slave_arr[$i]}"
-    echo $slave
-    ssh -l "root" "${slave_arr[$i]}" "${SCRIPT}" # error of alluxio: unknown command
+#one slow user
+slave="${slave_arr[5]}"
+SCRIPT="cd alluxio-la;PATH=$PATH python python/LABenchmark.py 0 $1 $2 > /tmp/pythonlog &"
+ssh -l "root" "${slave_arr[$i]}" "${SCRIPT}"
+
+#one fast user
+slave="${slave_arr[5]}"
+SCRIPT="cd alluxio-la;PATH=$PATH python python/LABenchmark.py 0 $1 $(($rate * $factor)) > /tmp/pythonlog &"
+echo $SCRIPT
+ssh -l "root" "${slave_arr[$i]}" "${SCRIPT}"
+
+
+#for ((i = 30; i < $1 + 30; i++))
+#do
+ #   echo $i
+ #   slave="${slave_arr[$i]}"
+ #   echo $slave
+ #   ssh -l "root" "${slave_arr[$i]}" "${SCRIPT}" # error of alluxio: unknown command
    
-done
+#done
 echo Done
