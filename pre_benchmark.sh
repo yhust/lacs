09f2d8d4e778e333bf685536f290f3b32e11fa4e
root <root@ip-172-31-75-165.ec2.internal>
2018-07-24 15:15:14 +0000

    downgrade pom of loadaware to java 1.7

--- pre_benchmark.sh
+++ pre_benchmark.sh
@@ -2,14 +2,14 @@
 
 
 
-$k=$1   # number of users
-$n=$2 # number of files
-$arrival_rate=$3 #  request rate of normal users(as Poisson)
-$zipf_factor=$4 # distribution
-$factor=$5 # ratio of request rates (aggressive to normal)
+k=$1   # number of users
+n=$2 # number of files
+arrival_rate=$3 #  request rate of normal users(as Poisson)
+zipf_factor=$4 # distribution
+factor=$5 # ratio of request rates (aggressive to normal)
 
 # Generate preference
-python python/generate_rates $k $n $arrival_rate $zipf_factor $factor
+python python/generate_rates.py $k $n $arrival_rate $zipf_factor $factor
 
 
 # Write test files
